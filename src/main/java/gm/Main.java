a17c971dde28ab9b8fdde2bc8ff593fc7259fc05
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-02-05 18:35:08 +0100

    Resolviendo conflicto en Main.java

--- src/main/java/gm/Main.java
+++ src/main/java/gm/Main.java
@@ -2,32 +2,7 @@ package gm;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        // Construyendo una pizza hawaiana
-        System.out.println("Construyendo una pizza Hawaiana...");
-        PizzaBuilder builder = new HawaiianPizzaBuilder().setSize("Large").setDough("Thin");
-        MestrePizzer mestre = new MestrePizzer(builder);
-        Pizza pizza = mestre.makePizza();
-        if (pizza != null) {
-            System.out.println("Pizza Hawaiana construida:");
-            System.out.println(pizza);
-        }
-
-        System.out.println();
-
-        // Construyendo una pizza vegetariana
-        System.out.println("Construyendo una pizza Vegetariana...");
-        PizzaBuilder builderVeg = new VegetarianPizzaBuilder().setSize("Medium").setDough("Thick");
-        MestrePizzer mestreVeg = new MestrePizzer(builderVeg);
-        Pizza vegetarianPizza = mestreVeg.makePizza();
-        if (vegetarianPizza != null) {
-            System.out.println("Pizza Vegetariana construida:");
-            System.out.println(vegetarianPizza);
-        }
-    }}
-=======
         Menu menu = new Menu();
         menu.mostrarMenu();
     }
 }
->>>>>>> 9c5c5eb (add menu y organización)

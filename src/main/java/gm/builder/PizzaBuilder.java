a59f00c25cb2f5415ddfbb968f340c981646c897
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-02-05 18:36:33 +0100

    Resolviendo conflicto en Main.java

--- src/main/java/gm/builder/PizzaBuilder.java
+++ src/main/java/gm/builder/PizzaBuilder.java
@@ -4,7 +4,6 @@ import gm.model.Pizza;
 
 import java.util.List;
 
-// Define métodos para configurar el tamaño, masa e ingredientes.
 public interface PizzaBuilder {
     PizzaBuilder setSize(String size);
     PizzaBuilder setDough(String dough);
